ae061bed03a70ceaf073b711cb73c60622742dce
Afiyaayat <37040878+sohelranacse@users.noreply.github.com>
2023-01-31 10:53:47 +0600

    update bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes, useLocation, Navigate } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 
 import Nav from './components/web/common/Nav';
 import Footer from './components/web/common/Footer';
